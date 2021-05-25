929f086ff8f3492f2ff39e28d809636c789df469
Admin <patilsharad2000@gmail.co>
2021-05-25 09:00:00 +0530

    ADDED : TestNG Listeners test is skipped

--- src/test/java/listeners/TestListenersDemo.java
+++ src/test/java/listeners/TestListenersDemo.java
@@ -1,6 +1,7 @@
 package listeners;
 
 import org.testng.Assert;
+import org.testng.SkipException;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
@@ -18,5 +19,6 @@ public class TestListenersDemo {
     @Test
     public void test3(){
         System.out.println("I am Inside test 3");
+        throw new SkipException("This test is skipped");
     }
 }
